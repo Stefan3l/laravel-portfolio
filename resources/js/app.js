4439db7fa82c0df79e2a9efa04b58d68513f5c6d
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2025-04-30 09:16:55 +0200

    feat: add laravel breeeze

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,7 @@
-import "./bootstrap";
-
-// import images
-import.meta.glob(["../images/**"]);
+import './bootstrap';
+import '~resources/scss/app.scss';
+import '~icons/bootstrap-icons.scss';
+import * as bootstrap from 'bootstrap';
+import.meta.glob([
+    '../img/**'
+])
